bba174392ce3b8e3c3e682ee3b5c21f8b2f4baab
Cesar Preza <cpreza93@yahoo.com>
2021-04-05 22:03:23 -0700

    form state

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,26 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Form, Button, Col, Row } from 'react-bootstrap';
 
-function LoginPage() {
-    return (
-        <div id='loginContainer'>
-            <Form id='loginForm'>
-                <Form.Group as={Row}>
-                    <Form.Label column sm={2}>Email</Form.Label>
-                    <Col sm={4}>
-                        <Form.Control type='email' placeholder='enter email' /> 
-                    </Col>
-                </Form.Group>
-                <Form.Group as={Row}>
-                    <Form.Label column sm={2}>Password</Form.Label>
-                    <Col sm={4}>
-                        <Form.Control type='password' />
-                    </Col>
-                </Form.Group>
-                <Button>Sign In</Button>
-            </Form>
-        </div>
-    )
+class LoginPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userEmail:'',
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit() {
+        console.log('submit form')
+    }
+
+    handleChange(e) {
+        this.setState({userEmail: e.target.value})
+    }
+
+    render() {
+        console.log(this.state.userEmail)
+        return (
+            <div id='loginContainer'>
+                <Form id='loginForm'>
+                    <Form.Group as={Row}>
+                        <Form.Label column sm={2}>Email</Form.Label>
+                        <Col sm={4}>
+                            <Form.Control onChange={this.handleChange} type='email' placeholder='enter email' /> 
+                        </Col>
+                    </Form.Group>
+                    <Form.Group as={Row}>
+                        <Form.Label column sm={2}>Password</Form.Label>
+                        <Col sm={4}>
+                            <Form.Control type='password' />
+                        </Col>
+                    </Form.Group>
+                    <Button onClick={this.handleSubmit}>Sign In</Button>
+                </Form>
+            </div>
+        )
+    }
 }
 
 export default LoginPage
