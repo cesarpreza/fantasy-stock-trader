2b6fefe8bbee29c36b1fad59addc1dadb2bd0555
Cesar Preza <cpreza93@yahoo.com>
2021-04-08 15:00:08 -0700

    mock table data portfolio.js

--- client/src/components/Portfolio.js
+++ client/src/components/Portfolio.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Card } from 'react-bootstrap';
 
 function Portfolio() {
     return (
@@ -12,6 +12,36 @@ function Portfolio() {
                     <p className='balances'>Account Balance: $100,000</p>
                     <p className='balances'>Total Holding Value: $0</p>
                 </Navbar>
+            </div>
+            <div>
+                <div className='holdings-container'>
+                    <h3>Current Holdings</h3>
+                </div>
+                <div>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Stocks</Card.Title>
+                                <table>
+                                    <thead>
+                                        <tr className='stock-info'>
+                                            <th>Stock Symbol</th>
+                                            <th>Price</th>
+                                            <th>Shares</th>
+                                            <th>Holding Value</th>
+                                            </tr>
+                                </thead>
+                                    <tbody>
+                                        <tr className='stock-info'>
+                                            <td>aapl</td>
+                                            <td>$130</td>
+                                            <td>2</td>
+                                            <td>$260</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                        </Card.Body>
+                    </Card>
+                </div>
             </div>
         </div>
     )
