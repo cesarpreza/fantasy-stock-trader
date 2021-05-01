e3fe2a31ed2c4760862f88f59729511eaa0e4d4d
Cesar Preza <cpreza93@yahoo.com>
2021-04-30 21:49:20 -0700

    adds user dummy data to backend

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const path = require('path');
-//const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 require('dotenv').config();
 const publicToken = process.env.PUBLIC_KEY;
 
@@ -11,6 +11,32 @@ app.use('/', express.static(path.join(__dirname, 'client/build')));
 
 app.listen(3000);
 console.log('listening on port 3000');
+
+let users = [
+    {
+        email: "abc@email.com",
+        password: "abc123"
+    }
+]
+
+    app.post('/api/auth', (req, res) => {
+        let userResult = users.find(user => user.email == req.body);
+        if (userResult) {
+            if (user.password === req.body.password) {
+                res.status(200).send({
+                    message: 'Password Matched'
+                })
+            } else {
+                res.status(200).send({
+                    message: 'Password incorrect'
+                })
+            }
+        } else {
+            res.status(200).send({
+                message: 'User not found'
+            })
+        }
+    });
 
 app.get('/api/stocks', (req, res) => {
     const stockName = req.query.stockName
