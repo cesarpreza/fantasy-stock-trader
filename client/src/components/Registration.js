249d34857d3cee93f7422760d1d8b705e3c27257
Cesar Preza <cpreza93@yahoo.com>
2021-07-11 11:07:35 -0700

    adds registration form

--- client/src/components/Registration.js
+++ client/src/components/Registration.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 function Registration() {
     return (
-        <div>
-            <h1>Register</h1>
+        <div className='registration-form'>
+            <Form>
+                <div className='form-group'>
+                    <label>First Name: </label>
+                    <input type='text' className='form-control-md' />
+                </div>
+                <div className='form-group'>
+                    <label>Last Name: </label>
+                    <input type='text' className='form-control-md' />
+                </div>
+                <div className='form-group'>
+                    <label>Username: </label>
+                    <input type='text' className='form-control-md' />
+                </div>
+                <div>
+                    <label>Email: </label>
+                    <input type='email' className='form-control-md' />
+                </div>
+            </Form>
+
+
+
+            <Button>Register!</Button>
         </div>
     )
 }
